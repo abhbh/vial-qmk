a1639d13b96d3951985bd8c418fed3c2ddec2f75
Abhinav <abhnv@abhnv.com>
2026-02-25 01:28:08 +0530

    pads to pins

--- keyboards/abhbh/sofle_choc_pico/config.h
+++ keyboards/abhbh/sofle_choc_pico/config.h
@@ -3,10 +3,10 @@
 /* key matrix size */
 #define TAPPING_TERM 100
 #define DEBOUNCE 5
-#define ENCODERS_PAD_A { GP28 }
-#define ENCODERS_PAD_B { GP29 }
-#define ENCODERS_PAD_A_RIGHT { GP29 }
-#define ENCODERS_PAD_B_RIGHT { GP28 }
+#define ENCODER_A_PINS { GP28 }
+#define ENCODER_B_PINS { GP29 }
+#define ENCODER_A_PINS_RIGHT { GP29 }
+#define ENCODER_B_PINS_RIGHT { GP28 }
 #define ENCODER_RESOLUTION 4
 
 #define TAP_CODE_DELAY 10
